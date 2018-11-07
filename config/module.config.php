6b9440e4a774923501782c21033522a6a020923c
Alex <alex@try2catch.com>
2018-11-07 15:44:18 +0100

    Added absoluteUrl alias to module config

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,8 @@
 <?php
 namespace Common;
 
+use Common\View\Helper\AbsoluteUrl;
+use Common\View\Helper\AbsoluteUrlFactory;
 use Common\View\Helper\StaticResource;
 use Common\View\Helper\StaticResourceFactory;
 
@@ -9,15 +11,17 @@ return [
 	'view_helpers' => [
 		'factories' => [
 			StaticResource::class => StaticResourceFactory::class,
+			AbsoluteUrl::class    => AbsoluteUrlFactory::class,
 		],
-		'aliases' => [
-			'staticResource' => StaticResource::class
+		'aliases'   => [
+			'staticResource' => StaticResource::class,
+			'absoluteUrl'    => AbsoluteUrl::class,
 		],
 	],
 
-	'service_manager'  => [
+	'service_manager' => [
 		'abstract_factories' => [
-			DefaultFactory::class
+			DefaultFactory::class,
 		],
 	],
 ];

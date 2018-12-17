d59aa2046250936805536d812f762a85d0db0a73
Alex Schloegl <alexander.schloegl@compass.at>
2018-12-17 12:54:49 +0100

    Added email property definition

---
+++ src/RequestData/PropertyDefinition/Email.php
@@ -0,0 +1,26 @@
+<?php
+namespace Common\RequestData\PropertyDefinition;
+
+use Zend\Validator\EmailAddress;
+
+class Email extends PropertyDefinition
+{
+	/**
+	 * @return Email
+	 */
+	public static function create()
+	{
+		return new self();
+	}
+
+	/**
+	 */
+	public function __construct()
+	{
+		$this->addValidator(
+			new EmailAddress()
+		);
+
+		parent::__construct();
+	}
+}
